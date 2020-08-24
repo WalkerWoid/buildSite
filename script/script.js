f941d1e2ce24816289de5462d51d69664f80a86b
walkerwoid <ghost2121warrior@mail.ru>
2020-08-24 19:44:55 +0400

    Первый push

---
+++ script/script.js
@@ -0,0 +1,24 @@
+class InputRange {
+    constructor() {
+        this.allRanges = [...document.querySelectorAll('.inputRange')];
+        this._init();
+    }
+    _init() {
+        this._eventHandlers();
+        console.log(this.allRanges);
+    }
+    _eventHandlers() {
+        for (const range of this.allRanges) {
+            range.addEventListener('input', () => this._changeRange(range, range.max));
+        }
+    }
+    _changeRange(range, max) {
+        let value = range.value;
+        let currentValue = ((value-range.min/2)*100)/max;
+        // let currentValue = Math.round(value*100/max);
+        range.style.background = `-webkit-linear-gradient(left, #F26422 0%, #F26422 ${currentValue}%, #D0D0D0 ${currentValue}%, #D0D0D0 100%)`;
+        // console.log(currentValue);
+        // console.log(value);
+    }
+}
+const inputRange = new InputRange();
